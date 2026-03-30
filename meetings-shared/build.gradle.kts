dddb3e17ee2b588ffe795d021c8ae721ce19a9bb
whysoezzy <whysoezzy@gmail.com>
2026-03-30 17:16:45 +0300

    Fix start app on iOS

--- meetings-shared/build.gradle.kts
+++ meetings-shared/build.gradle.kts
@@ -1,23 +1,34 @@
 plugins {
     alias(libs.plugins.jetbrains.kotlin.multiplatform)
-
+    alias(libs.plugins.jetbrains.compose.multiplatform)
+    alias(libs.plugins.jetbrains.compose.compiler)
 }
 
 kotlin {
     listOf(
+        iosX64(),
         iosArm64(),
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "ComposeApp"
+            baseName = "MeetingsShared"
             isStatic = true
         }
     }
 
     sourceSets {
-
         commonMain.dependencies {
-            api(projects.composeApp)
+            implementation(projects.composeApp)
+            // Необходимо явно добавить все зависимости для работы
+            // приложения на iOS, чтобы они попали в XCFramework
+            implementation(compose.runtime)
+            implementation(compose.ui)
+        }
+    }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.composeApp)
         }
     }
 }
--- meetings-shared/src/iosMain/kotlin/dev/whysoezzy/meetings/MainViewController.kt
+++ meetings-shared/src/iosMain/kotlin/dev/whysoezzy/meetings/MainViewController.kt
@@ -1,5 +1,6 @@
 package dev.whysoezzy.meetings
 
 import androidx.compose.ui.window.ComposeUIViewController
+import dev.whysoezzy.meetings.compose.MeetingsApp
 
 fun MainViewController() = ComposeUIViewController { MeetingsApp() }
