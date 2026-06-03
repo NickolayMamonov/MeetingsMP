2730332ca640e0c3877eb332c9d26912e8b2c9b0
whysoezzy <whysoezzy@gmail.com>
2026-06-03 04:20:56 +0300

    detekt: fix 4 detekt violations

--- composeApp/src/commonMain/kotlin/dev/whysoezzy/meetings/compose/MeetingsApp.kt
+++ composeApp/src/commonMain/kotlin/dev/whysoezzy/meetings/compose/MeetingsApp.kt
@@ -4,17 +4,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import uikit.MeetingsTheme
 
 @Composable
-@Preview
-fun MeetingsApp() {
+fun MeetingsApp(modifier: Modifier = Modifier) {
     MeetingsTheme{
         Surface(
-            Modifier.background(MeetingsTheme.colors.background)
+            modifier
+                .background(MeetingsTheme.colors.background)
                 .fillMaxSize()
         ) {
             Text("Hello, Meetings!")
